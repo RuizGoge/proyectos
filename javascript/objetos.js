c49878aaca51e56e5718f37d7f9414cee9a433ef
RuizGoge <ruizjorgeampa@gmail.com>
2021-03-04 01:32:50 -0300

    ultimo de hoy

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -1,15 +1,13 @@
 //Este reto consistió en realizar el ingreso de X cosas o personas atravez de una funcion asignar los valores de los datos de lo que se ingresará.
 
-
-
 function matricula(nombre, edad, sexo) {
     this.nombre = nombre;
     this.edad = edad;
     this.sexo = sexo;
 }
-let matriculados = [];
-let cantidadPersonas = prompt("Cuantas personas desea ingresar al sistema?"); //Para el reto serían 30.
 
+let cantidadPersonas = prompt("Cuantas personas desea ingresar al sistema?"); //Para el reto serían 30.
+let matriculados = [];
 for (let i = 0; i < cantidadPersonas; i++) {
     let n = prompt("Nombre");
     let e = prompt("Edad");
@@ -19,9 +17,8 @@ for (let i = 0; i < cantidadPersonas; i++) {
 
 
 //mostrarlos.
-
-
-for (let i = 0; i < matriculados.length; i++) {
-    console.log(matriculados[i]);
+let mostrarNombres = matriculados.map(function(personas){
+    return personas.nombre
     
-}
+})
+
